e2e321265ddde7c682dd68bea2aa5ed943f25998
Jagrutipyth13 <mahajanjagruti13@gmail.com>
2020-09-17 17:30:49 +0530

    sitka High complete plot

--- data/sitka_highs.py
+++ data/sitka_highs.py
@@ -1,5 +1,5 @@
 import csv
-
+import matplotlib.pyplot as plt
 filename = 'sitka_weather_07-2018_simple.csv'
 with open(filename,'r') as reader:
     reader = csv.reader(reader) #he csv module contains a next() function, which returns the next line
@@ -20,3 +20,16 @@ with open(filename,'r') as reader:
         high = int(row[5])
         highs.append(high)
     print(highs)
+
+
+# Plot the high temperatures.
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+# Format plot.
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
