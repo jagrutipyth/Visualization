288f1431b9afb2727ed867537ef448dc2cbee9bd
Jagrutipyth13 <mahajanjagruti13@gmail.com>
2020-09-18 11:31:47 +0530

    Added datetitme

--- data/sitka_highs.py
+++ data/sitka_highs.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 filename = 'sitka_weather_07-2018_simple.csv'
 with open(filename,'r') as reader:
     reader = csv.reader(reader) #he csv module contains a next() function, which returns the next line
@@ -9,15 +10,18 @@ with open(filename,'r') as reader:
 
     header_row = next(reader)
 
-    #print(header_row)
 
+    #print(header_row)
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
-# Get high temperatures from this file.
-    highs = []
+    # Get dates and high temperatures from this file.
+
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        dates.append(current_date)
         highs.append(high)
     print(highs)
 
@@ -25,11 +29,12 @@ with open(filename,'r') as reader:
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, c='red')
+ax.plot(dates, highs, c='red')
 
 # Format plot.
 plt.title("Daily high temperatures, July 2018", fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate() # draws date labels diagonally to prevent them from overlapping
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
